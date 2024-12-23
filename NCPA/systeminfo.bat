90b7ca674d919b1359196aef7f3e8d57c84b2cde
yang <joker_yang@foxmail.com>
2024-12-23 11:15:42 +0800

    Update systeminfo.bat

--- NCPA/systeminfo.bat
+++ NCPA/systeminfo.bat
@@ -2,13 +2,8 @@
 @chcp 437 >NUL
 
 set arg=%1
-if %arg%==system (
-    set key=Name:
-    systeminfo.exe|findstr /B OS|findstr Name:
-)
-if %arg%==kernel (
-    set key=Version:
-    systeminfo.exe|findstr /B OS|findstr Version:
+if %arg%==os (
+    systeminfo.exe|findstr /B OS|findstr "Name: Version:"
 )
 if %arg%==cpu (
     wmic cpu get numberoflogicalprocessors|findstr /V Number
